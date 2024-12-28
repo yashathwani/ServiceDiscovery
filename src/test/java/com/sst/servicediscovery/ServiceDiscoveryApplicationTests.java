fee202a23d8c0fe4696d4ef950f9d726a78030ec
Yash Athwani <yashathwani45@gmail.com>
2024-12-28 17:17:56 +0530

    created the eureka server

---
+++ src/test/java/com/sst/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sst.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
